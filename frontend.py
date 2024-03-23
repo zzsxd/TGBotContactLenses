07f4e7d12fe8f14636f0501a69b02db54e0d906d
zzsxd <nikitacapkov06@gmail.com>
2024-03-23 18:03:48 +0800

    add catalog, free lenses, about company informations, and add many button

--- frontend.py
+++ frontend.py
@@ -22,3 +22,132 @@ class Bot_inline_btns:
         catalog = types.KeyboardButton('Каталог')
         keyboard.add(stock, company, free, notification, catalog)
         return keyboard
+
+    def admin_btns(self):
+        one = types.InlineKeyboardButton('Экспорт БД', callback_data='export')
+        self.__markup.add(one)
+        return self.__markup
+
+    def registration_btns(self):
+        reg = types.InlineKeyboardButton('Зарегестрироваться!', url='https://www.illusion-lens.ru/product/besplatnaya-para-linz-illusion-aero-light/')
+        self.__markup.add(reg)
+        return self.__markup
+
+    def about_btns(self):
+        magasine = types.InlineKeyboardButton('Интернет-магазин', url='illusion-lens.ru')
+        contact = types.InlineKeyboardButton('Контактные данные', callback_data='condata')
+        self.__markup.add(magasine, contact)
+        return self.__markup
+
+
+    def catalog_btns(self):
+        transparent = types.InlineKeyboardButton('Прозрачные линзы', callback_data='transparent')
+        blue = types.InlineKeyboardButton('Голубые линзы', callback_data='blue')
+        green = types.InlineKeyboardButton('Зеленые линзы', callback_data='green')
+        gray = types.InlineKeyboardButton('Серые линзы', callback_data='gray')
+        black = types.InlineKeyboardButton('Черные линзы', callback_data='black')
+        brown = types.InlineKeyboardButton('Карие линзы', callback_data='brown')
+        violet = types.InlineKeyboardButton('Фиолетовые линзы', callback_data='violet')
+        carnaval = types.InlineKeyboardButton('Карнавальные линзы', callback_data='carnaval')
+        solutions = types.InlineKeyboardButton('Растворы и аксессуары', callback_data='solutions')
+        self.__markup.add(transparent, blue, green, gray, black, brown, violet, carnaval, solutions)
+        return self.__markup
+
+    def transparent_btns(self):
+        aero = types.InlineKeyboardButton('Illusion Aero', callback_data="aero")
+        aero_light = types.InlineKeyboardButton('Illusion Aero Light', callback_data="light")
+        test_illusion = types.InlineKeyboardButton('Тестовый набор Illusion', callback_data="test")
+        clear = types.InlineKeyboardButton('Illusion Clear (4 блистера)', callback_data='clear')
+        clear2 = types.InlineKeyboardButton('Illusion Clear (2 блистера)', callback_data='clear2')
+        fashion = types.InlineKeyboardButton('Illusion Fashion', callback_data='fashion')
+        self.__markup.add(aero, aero_light, test_illusion, clear, clear2, fashion)
+        return self.__markup
+
+    def blue_lenses_btns(self):
+        glowblue = types.InlineKeyboardButton('Illusion Colors Glow', callback_data='glowblue')
+        eleganceblue = types.InlineKeyboardButton('Illusion Colors Elegance', callback_data='eleganceblue')
+        geodiamondblue = types.InlineKeyboardButton('Illusion Geo Diamond', callback_data='geodiamondblue')
+        geomagicblue = types.InlineKeyboardButton('Illusion Geo Magic', callback_data='geomagicblue')
+        geonatureblue = types.InlineKeyboardButton('Illusion Geo Nature', callback_data='geonatureblue')
+        luxeblue = types.InlineKeyboardButton('Illusion Luxe', callback_data='luxeblue')
+        shineblue = types.InlineKeyboardButton('Illusion Shine', callback_data='shineblue')
+        fashionadonisblue = types.InlineKeyboardButton('Illusion Fashion Adonis', callback_data='fashionadonisblue')
+        self.__markup.add(glowblue, eleganceblue, geodiamondblue, geomagicblue, geonatureblue, luxeblue, shineblue, fashionadonisblue)
+        return self.__markup
+
+    def green_lenses_btns(self):
+        glowgreen = types.InlineKeyboardButton('Illusion Colors Glow', callback_data='glowgreen')
+        elegancegreen = types.InlineKeyboardButton('Illusion Colors Elegance', callback_data='elegancegreen')
+        geodiamondgreen = types.InlineKeyboardButton('Illusion Geo Diamond', callback_data='geodiamondgreen')
+        geomagicgreen = types.InlineKeyboardButton('Illusion Geo Magic', callback_data='geomagicgreen')
+        geonaturegreen = types.InlineKeyboardButton('Illusion Geo Nature', callback_data='geonaturegreen')
+        luxegreen = types.InlineKeyboardButton('Illusion Luxe', callback_data='luxegreen')
+        shinegreen = types.InlineKeyboardButton('Illusion Shine', callback_data='shinegreen')
+        fashionadonisgreen = types.InlineKeyboardButton('Illusion Fashion Adonis', callback_data='fashionadonisgreen')
+        fashionluxegreen = types.InlineKeyboardButton('Illusion Fashion Luxe', callback_data='fashionluxegreen')
+        self.__markup.add(glowgreen, elegancegreen, geodiamondgreen, geomagicgreen, geonaturegreen, luxegreen, shinegreen, fashionadonisgreen, fashionluxegreen)
+        return self.__markup
+
+    def gray_lenses_btns(self):
+        glowgray = types.InlineKeyboardButton('Illusion Colors Glow', callback_data='glowgray')
+        elegancegray = types.InlineKeyboardButton('Illusion Colors Elegance', callback_data='elegancegray')
+        colorshinegray = types.InlineKeyboardButton('Illusion Colors Shine', callback_data='geodiamondgray')
+        geomagicgray = types.InlineKeyboardButton('Illusion Geo Magic', callback_data='geomagicgray')
+        geonaturegray = types.InlineKeyboardButton('Illusion Diamond', callback_data='geonaturegray')
+        adonisgray = types.InlineKeyboardButton('Illusion Fashion Adonis', callback_data='luxegray')
+        luxegray = types.InlineKeyboardButton('Illusion Fashion Luxe', callback_data='shinegray')
+        self.__markup.add(glowgray, elegancegray, colorshinegray, geomagicgray, geonaturegray, adonisgray, luxegray)
+        return self.__markup
+
+    def black_lenses_btns(self):
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        colorshineblack = types.InlineKeyboardButton('Illusion Colors Shine', callback_data='colorshineblack')
+        adonisblack = types.InlineKeyboardButton('Illusion Fashion Adonis', callback_data='fashionadonisblack')
+        self.__markup.add(glowblack, colorshineblack, adonisblack)
+        return self.__markup
+
+    def brown_lenses_btns(self):
+        elegancebrown = types.InlineKeyboardButton('Illusion Colors Elegance', callback_data='elegancebrown')
+        colorshinebrown = types.InlineKeyboardButton('Illusion Colors Shine', callback_data='colorshinebrown')
+        geodiamondbrown = types.InlineKeyboardButton('Illusion Geo Diamond', callback_data='geodiamondbrown')
+        adonisbrown = types.InlineKeyboardButton('Illusion Fashion Adonis', callback_data='luxebrown')
+        luxebrown = types.InlineKeyboardButton('Illusion Fashion Luxe', callback_data='shinebrown')
+        self.__markup.add(elegancebrown, colorshinebrown, geodiamondbrown, adonisbrown, luxebrown)
+        return self.__markup
+
+    def violet_lenses_btns(self):
+        eleganceviolet = types.InlineKeyboardButton('Illusion Colors Elegance', callback_data='eleganceviolet')
+        colorshineviolet = types.InlineKeyboardButton('Illusion Colors Shine', callback_data='colorshineviolet')
+        adonisviolet = types.InlineKeyboardButton('Illusion Fashion Adonis', callback_data='luxeviolet')
+        luxeviolet = types.InlineKeyboardButton('Illusion Fashion Luxe', callback_data='shineviolet')
+        self.__markup.add(eleganceviolet, colorshineviolet, adonisviolet, luxeviolet)
+        return self.__markup
+
+    def carnaval_lenses_btns(self):
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
+        glowblack = types.InlineKeyboardButton('Illusion Colors Rio', callback_data='colorsrioblack')
--- main.py
+++ main.py
@@ -17,22 +17,49 @@ def main():
         buttons = Bot_inline_btns()
         command = message.text.replace('/', '')
         if command =='start':
-            bot.send_message(message.chat.id, 'Стартовое сообщение', reply_markup=buttons.start_btns())
+            bot.send_message(message.chat.id, 'ILLUSION контактные линзы нового поколения!', reply_markup=buttons.start_btns())
         elif command =='admin':
-            bot.send_message(message.chat.id, 'Админ панель')
+            bot.send_message(message.chat.id, f'{message.from_user.first_name}, вы успешно вошли в Админ-Панель!', reply_markup=buttons.admin_btns())
     @bot.message_handler(content_types=['text'])
     def text_msg(message):
         buttons = Bot_inline_btns()
         if message.text == 'Все акции':
             bot.send_message(message.chat.id, 'Что-то про акции')
         elif message.text == 'О компании':
-            bot.send_message(message.chat.id, 'Что-то про компанию')
+            bot.send_message(message.chat.id, 'ILLUSION контактные линзы нового поколения!\n'
+                                                    '\n'
+                                                    'С 2012 года компания «Вижен Трейд» является эксклюзивным дистрибьютером контактных линз бренда ILLUSION.\n'
+                                                    '\n'
+                                                    'Безопасные современные контактные линзы нового поколения ILLUSION представлены в различной цветовой гамме с разнообразным дизайном и востребованы в кабинетах контактной коррекции, интернет - магазинах, аптеках, оптиках и в фармацевтических компаниях.\n'
+                                                    '\n', reply_markup=buttons.about_btns())
         elif message.text == 'Бесплатные линзы':
-            bot.send_message(message.chat.id, 'Что-то про бесплатные линзы')
+            bot.send_message(message.chat.id, 'Получи бесплатную пару линз ILLUSION Aero Light.\n'
+                                              '\n'
+                                              'Регистрируйся на сайте, добавь тестовую пару линз в корзину, дождись доставку и наслаждайся идеальным зрением!\n'
+                                              '\n'
+                                              'Двухнедельные линзы ILLUSION Aero Light сделаны из инновационного материала — тонкие и упругие, они практически не чувствуются на глазах.\n'
+                                              '\n'
+                                              'ILLUSION Aero Light – это силикон-гидрогелевые линзы с высокой кислородопроницаемостью и повышенным влагосодержанием. Асферический дизайн линзы обеспечивает чёткое зрение даже при низкой освещенности.\n'
+                                              '\n'
+                                              'Тестируй ILLUSION Aero Light бесплатно!', reply_markup=buttons.registration_btns())
         elif message.text == 'Напоминание':
             bot.send_message(message.chat.id, 'Что-то про напоминание')
         elif message.text == 'Каталог':
-            bot.send_message(message.chat.id, 'Что-то про каталог')
+            bot.send_message(message.chat.id, 'Каталог', reply_markup=buttons.catalog_btns())
+
+
+    @bot.callback_query_handler(func=lambda call:True)
+    def callback(call):
+        buttons = Bot_inline_btns()
+        if call.data == 'transparent':
+            bot.send_message(call.message.chat.id, 'Прозрачные линзы', reply_markup=buttons.transparent_btns())
+        elif call.data == '':
+            pass
+        elif call.data == 'condata':
+            bot.send_message(call.message.chat.id, 'Группа ВК: vk.com/illusion_lens\n\n'
+                                                   'Электронный адрес: info@illusion-lens.ru\n\n'
+                                                   'Телефон для заказа через сайт или каталог товаров: 8 (812) 326 32 21 (Whatsapp, Telegram, Viber)\n\n'
+                                                   'Время работы колл-центра: 9:30-18:00 Пн-Пт')
     bot.polling(none_stop=True)
 
 
