c1f6ae0b74ea702ebdef8cac18a70e86c9b35d2f
zzsxd <nikitacapkov06@gmail.com>
2024-03-26 17:44:14 +0800

    add db "users" and export data base to xlsx

--- backend.py
+++ backend.py
@@ -4,6 +4,9 @@
 #####################################
 import os
 import time
+import pandas as pd
+import csv
+
 #####################################
 
 class TempUserData:
@@ -15,3 +18,53 @@ class TempUserData:
         if user_id not in self.__user_data.keys():
             self.__user_data.update({user_id: [None, None, None]})
         return self.__user_data
+
+
+class DbAct:
+    def __init__(self, db, config):
+        super(DbAct, self).__init__()
+        self.__db = db
+        self.__config = config
+        self.__fields = ['Имя', 'Фамилия', 'Никнейм']
+        self.__dump_path_xlsx = path_xlsx
+
+    def add_user(self, user_id, first_name, last_name, nick_name):
+        if not self.user_is_existed(user_id):
+            if user_id in self.__config.get_config()['admins']:
+                is_admin = True
+            else:
+                is_admin = False
+            self.__db.db_write(
+                'INSERT INTO users (user_id, first_name, last_name, nick_name, is_admin) VALUES (?, ?, ?, ?, ?)',
+                (user_id, first_name, last_name, nick_name, is_admin))
+
+    def user_is_existed(self, user_id):
+        data = self.__db.db_read('SELECT count(*) FROM users WHERE user_id = ?', (user_id,))
+        if len(data) > 0:
+            if data[0][0] > 0:
+                status = True
+            else:
+                status = False
+            return status
+
+    def user_is_admin(self, user_id):
+        data = self.__db.db_read('SELECT is_admin FROM users WHERE user_id = ?', (user_id,))
+        if len(data) > 0:
+            if data[0][0] == 1:
+                status = True
+            else:
+                status = False
+            return status
+
+    def db_export_xlsx(self):
+        d = {'Имя': [], 'Фамилия': [], 'Никнейм': []}
+        users = self.__db.db_read('SELECT first_name, last_name, nick_name FROM users', ())
+        if len(users) > 0:
+            for user in users:
+                for info in range(len(list(user))):
+                    d[self.__fields[info]].addend(user[info])
+                for i in range(3):
+                    for j in range(3):
+                        d[self.__fields[j]].append(None)
+            df = pd.DataFrame(d)
+            df.to_excel(self.__dump_path_xlsx, sheet_name='пользователи', index=False)
--- main.py
+++ main.py
@@ -3,69 +3,83 @@
 #               zzsxd               #
 #####################################
 config_name = 'secrets.json'
-reminders = {} #словарь для хранения напоминаний для каждого пользователя
+reminders = {}  # словарь для хранения напоминаний для каждого пользователя
 #####################################
 import os
 import telebot
 import platform
 from datetime import datetime, timedelta
 import threading
+from threading import Lock
 import time
 from config_parser import ConfigParser
 from frontend import Bot_inline_btns
-from backend import TempUserData
-
+from backend import TempUserData, DbAct
+from db import DB
 
 
 def main():
     @bot.message_handler(commands=['start', 'admin'])
     def start_msg(message):
+        name_user = message.from_user.first_name
+        user_id = message.from_user.id
         buttons = Bot_inline_btns()
         command = message.text.replace('/', '')
-        if command =='start':
-            bot.send_message(message.chat.id, 'ILLUSION контактные линзы нового поколения!', reply_markup=buttons.start_btns())
-        elif command =='admin':
-            bot.send_message(message.chat.id, f'{message.from_user.first_name}, вы успешно вошли в Админ-Панель!', reply_markup=buttons.admin_btns())
+        db_actions.add_user(user_id, message.from_user.first_name, message.from_user.last_name,
+                            f'@{message.from_user.username}')
+        if command == 'start':
+            bot.send_message(message.chat.id, 'ILLUSION контактные линзы нового поколения!',
+                             reply_markup=buttons.start_btns())
+        elif db_actions.user_is_admin(user_id):
+            if command == 'admin':
+                bot.send_message(message.chat.id, f'{message.from_user.first_name}, вы успешно вошли в Админ-Панель!',
+                                 reply_markup=buttons.admin_btns())
+
     @bot.message_handler(content_types=['text'])
     def text_msg(message):
         user_id = message.chat.id
         buttons = Bot_inline_btns()
         code = temp_user_data.temp_data(user_id)[user_id][0]
-        if message.text == 'Акции':
-            bot.send_message(message.chat.id, 'Наши акции:', reply_markup=buttons.actions_btns())
-        elif message.text == 'О компании':
-            bot.send_message(message.chat.id, 'ILLUSION контактные линзы нового поколения!\n'
-                                                    '\n'
-                                                    'С 2012 года компания «Вижен Трейд» является эксклюзивным дистрибьютером контактных линз бренда ILLUSION.\n'
-                                                    '\n'
-                                                    'Безопасные современные контактные линзы нового поколения ILLUSION представлены в различной цветовой гамме с разнообразным дизайном и востребованы в кабинетах контактной коррекции, интернет - магазинах, аптеках, оптиках и в фармацевтических компаниях.\n'
-                                                    '\n', reply_markup=buttons.about_btns())
-        elif message.text == 'Бесплатные линзы':
-            bot.send_message(message.chat.id, 'Получи бесплатную пару линз ILLUSION Aero Light.\n'
-                                              '\n'
-                                              'Регистрируйся на сайте, добавь тестовую пару линз в корзину, дождись доставку и наслаждайся идеальным зрением!\n'
-                                              '\n'
-                                              'Двухнедельные линзы ILLUSION Aero Light сделаны из инновационного материала — тонкие и упругие, они практически не чувствуются на глазах.\n'
-                                              '\n'
-                                              'ILLUSION Aero Light – это силикон-гидрогелевые линзы с высокой кислородопроницаемостью и повышенным влагосодержанием. Асферический дизайн линзы обеспечивает чёткое зрение даже при низкой освещенности.\n'
-                                              '\n'
-                                              'Тестируй ILLUSION Aero Light бесплатно!', reply_markup=buttons.registration_btns())
-        elif message.text == 'Напоминание':
-            bot.send_message(message.chat.id, 'Вы можете поставить себе напоминание, чтобы не забыть купить новые линзы!\n\n'
-                                              'Введите дату, тогда бот пришлет вам напоминание!\n\n(Дата в формате: DD.MM.YYYY HH:MM (16.03.24 16:00)')
-            temp_user_data.temp_data(user_id)[user_id][0] = 0
-        elif code == 0:
-            if message.text and ' ' in message.text:
-                try:
-                    remind_time = datetime.strptime(message.text, '%d.%m.%Y %H:%M')
-                    if user_id not in reminders:
-                        reminders[user_id] = []
-                    reminders[user_id].append((message.text, remind_time))
-                    bot.reply_to(message, f"Напоминание на {remind_time.strftime('%d.%m.%Y %H:%M')} сохранено")
-                except ValueError:
-                    bot.reply_to(message, "Неверный формат. Используйте 'DD.MM.YYYY HH:MM'")
-        elif message.text == 'Каталог':
-            bot.send_message(message.chat.id, 'Каталог', reply_markup=buttons.catalog_btns())
+        if db_actions.user_is_existed(user_id):
+            if message.text == 'Акции':
+                bot.send_message(message.chat.id, 'Наши акции:', reply_markup=buttons.actions_btns())
+            elif message.text == 'О компании':
+                bot.send_message(message.chat.id, 'ILLUSION контактные линзы нового поколения!\n'
+                                                  '\n'
+                                                  'С 2012 года компания «Вижен Трейд» является эксклюзивным дистрибьютером контактных линз бренда ILLUSION.\n'
+                                                  '\n'
+                                                  'Безопасные современные контактные линзы нового поколения ILLUSION представлены в различной цветовой гамме с разнообразным дизайном и востребованы в кабинетах контактной коррекции, интернет - магазинах, аптеках, оптиках и в фармацевтических компаниях.\n'
+                                                  '\n', reply_markup=buttons.about_btns())
+            elif message.text == 'Бесплатные линзы':
+                bot.send_message(message.chat.id, 'Получи бесплатную пару линз ILLUSION Aero Light.\n'
+                                                  '\n'
+                                                  'Регистрируйся на сайте, добавь тестовую пару линз в корзину, дождись доставку и наслаждайся идеальным зрением!\n'
+                                                  '\n'
+                                                  'Двухнедельные линзы ILLUSION Aero Light сделаны из инновационного материала — тонкие и упругие, они практически не чувствуются на глазах.\n'
+                                                  '\n'
+                                                  'ILLUSION Aero Light – это силикон-гидрогелевые линзы с высокой кислородопроницаемостью и повышенным влагосодержанием. Асферический дизайн линзы обеспечивает чёткое зрение даже при низкой освещенности.\n'
+                                                  '\n'
+                                                  'Тестируй ILLUSION Aero Light бесплатно!',
+                                 reply_markup=buttons.registration_btns())
+            elif message.text == 'Напоминание':
+                bot.send_message(message.chat.id,
+                                 'Вы можете поставить себе напоминание, чтобы не забыть купить новые линзы!\n\n'
+                                 'Введите дату, тогда бот пришлет вам напоминание!\n\n(Дата в формате: DD.MM.YYYY HH:MM (16.03.24 16:00)')
+                temp_user_data.temp_data(user_id)[user_id][0] = 0
+            elif code == 0:
+                if message.text and ' ' in message.text:
+                    try:
+                        remind_time = datetime.strptime(message.text, '%d.%m.%Y %H:%M')
+                        if user_id not in reminders:
+                            reminders[user_id] = []
+                        reminders[user_id].append((message.text, remind_time))
+                        bot.reply_to(message, f"Напоминание на {remind_time.strftime('%d.%m.%Y %H:%M')} сохранено")
+                    except ValueError:
+                        bot.reply_to(message, "Неверный формат. Используйте 'DD.MM.YYYY HH:MM'")
+            elif message.text == 'Каталог':
+                bot.send_message(message.chat.id, 'Каталог', reply_markup=buttons.catalog_btns())
+        else:
+            bot.send_message(message.chat.id, 'Введите /start для запуска бота')
 
     def check_reminders():
         while True:
@@ -80,10 +94,11 @@ def main():
                 reminders[chat_id] = new_list
             # Проверяем каждую минуту
             time.sleep(60)
+
     reminder_thread = threading.Thread(target=check_reminders)
     reminder_thread.start()
 
-    @bot.callback_query_handler(func=lambda call:True)
+    @bot.callback_query_handler(func=lambda call: True)
     def callback(call):
         buttons = Bot_inline_btns()
         if call.data == 'transparent':
@@ -117,15 +132,19 @@ def main():
                                                    'Время работы колл-центра: 9:30-18:00 Пн-Пт')
         elif call.data == 'freelenses':
             bot.send_message(call.message.chat.id, 'Получи бесплатную пару линз ILLUSION Aero Light.\n'
-                                              '\n'
-                                              'Регистрируйся на сайте, добавь тестовую пару линз в корзину, дождись доставку и наслаждайся идеальным зрением!\n'
-                                              '\n'
-                                              'Двухнедельные линзы ILLUSION Aero Light сделаны из инновационного материала — тонкие и упругие, они практически не чувствуются на глазах.\n'
-                                              '\n'
-                                              'ILLUSION Aero Light – это силикон-гидрогелевые линзы с высокой кислородопроницаемостью и повышенным влагосодержанием. Асферический дизайн линзы обеспечивает чёткое зрение даже при низкой освещенности.\n'
-                                              '\n'
-                                              'Тестируй ILLUSION Aero Light бесплатно!',
+                                                   '\n'
+                                                   'Регистрируйся на сайте, добавь тестовую пару линз в корзину, дождись доставку и наслаждайся идеальным зрением!\n'
+                                                   '\n'
+                                                   'Двухнедельные линзы ILLUSION Aero Light сделаны из инновационного материала — тонкие и упругие, они практически не чувствуются на глазах.\n'
+                                                   '\n'
+                                                   'ILLUSION Aero Light – это силикон-гидрогелевые линзы с высокой кислородопроницаемостью и повышенным влагосодержанием. Асферический дизайн линзы обеспечивает чёткое зрение даже при низкой освещенности.\n'
+                                                   '\n'
+                                                   'Тестируй ILLUSION Aero Light бесплатно!',
                              reply_markup=buttons.registration_btns())
+        elif call.data == 'export':
+            db_actions.db_export_xlsx()
+            bot.send_document(call.message.chat.id, open(xlsx_path), 'rb'))
+            os.remove(xlsx_path)
 
     bot.polling(none_stop=True)
 
@@ -135,5 +154,7 @@ if '__main__' == __name__:
     work_dir = os.path.dirname(os.path.realpath(__file__))
     config = ConfigParser(f'{work_dir}/{config_name}', os_type)
     temp_user_data = TempUserData()
+    db = DB(config.get_config()['db_file_name'], Lock())
+    db_actions = DbAct(db, config)
     bot = telebot.TeleBot(config.get_config()['tg_api'])
     main()
